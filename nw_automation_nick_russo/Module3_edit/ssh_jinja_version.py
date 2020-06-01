2881312641ca205eff2b4efce4408ee0b6cfcef0
saptbane <saptbane@cisco.com>
2020-06-01 12:45:07 -0400

    testing commit with smtp for core files on FTD

--- nw_automation_nick_russo/Module3_edit/ssh_jinja_version.py
+++ nw_automation_nick_russo/Module3_edit/ssh_jinja_version.py
@@ -1,8 +1,17 @@
 import time
 import paramiko
 import yaml
+import smtplib
+import re
 #from PyYAML import yaml
 from jinja2 import Environment, FileSystemLoader
+email = 'saptbane@cisco.com'
+
+def send_email(email,host):
+    smtpObj = smtplib.SMTP('outbound.cisco.com', 25)
+    smtpObj.ehlo()
+    smtpObj.sendmail('noreply@cisco.com',email, 'Subject:"Core Found on" + host')
+    smtpObj.quit()
 
 def send_cmd(conn, command):
     conn.send(command+ "\n")
@@ -10,6 +19,14 @@ def send_cmd(conn, command):
 
 def get_output(conn):
     return conn.recv(65535).decode("utf-8")
+
+def is_core(output,host):
+    model_regex = re.compile(r"core_(?P<file>\S+)")
+    model_match = model_regex.search(output)
+    if model_match:
+        email_subject = "Core file "+(model_match.group("file"))+" found on "+host
+        print (email_subject)
+        send_email(email, host)
 
 def main():
 
@@ -20,10 +37,10 @@ def main():
      #       "10.2.4.23": "Admin123"
      #   }
     with open("host_yaml/host.yml", "r") as hosts:
-        host_root = yaml.load(hosts)
+        host_root = yaml.load(hosts, Loader=yaml.FullLoader)
 
     for host in host_root["host_list"]:
-        print (host)
+        #print (host)
         # paramiko can be client or server, we are using client here
         conn_param = paramiko.SSHClient()
 
@@ -34,18 +51,26 @@ def main():
         conn = conn_param.invoke_shell()
         time.sleep(2.0)
         #print ("test")
+        send_cmd(conn, "expert")
 
         # testing this using only 1 command and will move to a list when testing multiple commands
         commands = [
             #"show version",
-            "expert",
-            "ls /ngfw/var/common",
+            #"expert",
+            "ls /ngfw/var/common | grep core",
         ]
         for command in commands:
             #time.sleep(1.0)
             send_cmd(conn, command)
             time.sleep(1.0)
-            print(get_output(conn))
+            result = (get_output(conn))
+            is_core(result,host["ip"])
+
+            # model_regex = re.compile(r"core_(?P<file>\S+)")
+            # model_match = model_regex.search(result)
+            # if model_match:
+            #     print (model_match.group("file"))
+                #break
 
         conn.close()
 
